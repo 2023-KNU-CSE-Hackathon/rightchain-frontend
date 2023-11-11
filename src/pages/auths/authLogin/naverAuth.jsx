e96aa4c6728a421e0caf50428efad31974b28491
gusdndl <gusdndl03@gmail.com>
2023-11-12 01:33:32 +0900

    solve Error

--- src/pages/auths/authLogin/naverAuth.jsx
+++ src/pages/auths/authLogin/naverAuth.jsx
@@ -3,11 +3,39 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+const CLIENT_ID = "Zn5JnI_lZiRttfFL7uTR";
+const REDIRECT_URI = "http://localhost:5173";
+const STATE = "RightChain";
+export const NAVER_AUTH_URI = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&state=${STATE}`;
+
+export const fetchNaverTokens = async () => {
+  const code = new URL(window.location.href).searchParams.get("code");
+  const state = new URL(window.location.href).searchParams.get("state");
+
+  if (code && state) {
+    try {
+        const toeknResponse = await axios.post(
+            "http://localhost:50001/api/v1/oauth/naver/login/callback/",
+            {code, state}
+        );
+
+        const accessToken = tokenResponse.data.access_token;
+        const refreshToken = tokenResponse.data.refresh_token;
+
+        localStorage.setItem("accessToken", accessToken);
+        localStorage.setItem("refreshToken", refreshToken);
+
+        return true;
+    } catch (error) {
+        console.error("Error:", error);
+        return false;
+    }
+  } 
+
+  return false;
+};
+
 const NaverLogin = () => {
-  const CLIENT_ID = "Zn5JnI_lZiRttfFL7uTR";
-  const REDIRECT_URI = "http://localhost:5173";
-  const STATE = "RightChain";
-  const NAVER_AUTH_URI = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&state=${STATE}`;
 
   const [loading, setLoading] = useState(true);
 
@@ -17,33 +45,6 @@ const NaverLogin = () => {
   useEffect(() => {
     // 인가코드 받아오기
     const code = new URL(window.location.href).searchParams.get("code");
-
-    const fetchNaverTokens = async () => {
-      const code = new URL(window.location.href).searchParams.get("code");
-      const state = new URL(window.location.href).searchParams.get("state");
-
-      if (code && state) {
-        try {
-            const toeknResponse = await axios.post(
-                "http://localhost:50001/api/v1/oauth/naver/login/callback/",
-                {code, state}
-            );
-
-            const accessToken = tokenResponse.data.access_token;
-            const refreshToken = tokenResponse.data.refresh_token;
-
-            localStorage.setItem("accessToken", accessToken);
-            localStorage.setItem("refreshToken", refreshToken);
-
-            return true;
-        } catch (error) {
-            console.error("Error:", error);
-            return false;
-        }
-      } 
-
-      return false;
-    };
 
     if (code) {
       fetchTokens();
